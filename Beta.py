517f4d7c8e58cb2eb13f4cf7a01f34767dbeedaf
Mr. Beta <94112333+Mr-Beta-Version@users.noreply.github.com>
2023-07-10 17:44:16 +0600

    Update Beta.py

--- Beta.py
+++ Beta.py
@@ -1,14 +1,38 @@
 __import__('os').system('git pull')
 __import__('os').system('clear')
+def menu():
+    __import__('os').system('clear')
+    print('1. Cookies Remove')
+    print('2. Number Remove')
+    choice = input('>> ')
+    if choice=='1':rc()
+    elif choice=='2':rn()
+    else:menu()
 
-try:all_id = open(input('Input File Path > '),'r').read().splitlines()
-except:exit('File Not Valid')
+def rc():
+    print('Remove Cookies Selected')
+    try:all_id = open(input('Input File Path > '),'r').read().splitlines()
+    except:exit('File Not Valid')
+    for id in all_id:
+        try:
+            uid,password,cookie = id.split('|')
+            print(uid + '|' + password)
+            open('/sdcard/IDS.txt','a').write(uid+'|'+password+'\n')
+            open('/sdcard/cookies.txt','a').write(cookie+'\n')
+        except:
+           pass
 
-for id in all_id:
-    try:
-        uid,password,cookie = id.split('|')
-        print(uid + '|' + password)
-        open('/sdcard/IDS.txt','a').write(uid+'|'+password+'\n')
-        open('/sdcard/cookies.txt','a').write(cookie+'\n')
-    except:
-        pass
+def rn():
+    print('Remove Number Selected')
+    try:all_id = open(input('Input File Path > '),'r').read().splitlines()
+    except:exit('File Not Valid')
+    for id in all_id:
+        try:
+            number,uid,password,cookie = id.split('|')
+            print(uid + '|' + password)
+            open('/sdcard/IDS.txt','a').write(uid+'|'+password+'|'+cookie+'\n')
+        except:
+           pass
+            
+if __name__=='__main__':
+    menu()
